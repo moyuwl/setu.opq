e1a525658826020c6162746645774e72c6b0533d
Rocks Wang <rockswang@gmail.com>
2020-09-10 15:43:47 +0800

    Update setu.lua

--- setu.lua
+++ setu.lua
@@ -3,6 +3,17 @@ local Api = require("coreApi")
 local json = require("json")
 local http = require("http")
 
+local whitelist = { 757360354 } -- 只有白名单里的群中可以使用色图指令
+
+local function contains(table, val)
+   for i = 1, #table do
+      if table[i] == val then 
+         return true
+      end
+   end
+   return false
+end
+
 function ReceiveFriendMsg(CurrentQQ, data)
     return 1
 end
@@ -10,16 +21,19 @@ function ReceiveGroupMsg(CurrentQQ, data)
     if (string.find(data.Content, "涩图") ~= 1 and string.find(data.Content, "色图") ~= 1 and string.find(data.Content, "setu") ~= 1) then
         return 1
     end
+    if not contains(whitelist, data.FromGroupId) then
+        return 1
+    end
     
-    local idx = math.random(0, 95)
-    idx = string.format('%04d', idx)
-    local url = "https://cdn.jsdelivr.net/gh/ipchi9012/setu_pics@latest/setu_" .. idx .. ".js"
+    local idx = math.random(0, 95) -- 有效的索引从0000 ~ 0095
+    idx = string.format('%04d', idx) -- 前面填充0,
+    local url = "https://cdn.jsdelivr.net/gh/ipchi9012/setu_pics@latest/setu_" .. idx .. ".js" -- 索引文件地址
     log.info("url=%s", url)
     local resp = http.request("GET", url).body
     idx = string.find(resp, "(", 1, true)
-    resp = string.sub(resp, idx + 1, -2)
-		resp = json.decode(resp)
-    local item = resp[math.random(table.getn(resp))]
+    resp = string.sub(resp, idx + 1, -2) -- 去掉前面的setu_xxxx(和后面的)
+    resp = json.decode(resp)
+    local item = resp[math.random(#resp)] -- 从数组中随机选取一个元素
     local url = "https://cdn.jsdelivr.net/gh/ipchi9012/setu_pics@latest/" .. item.path
     local text = "『" .. item.title .. "』 作者：" .. item.author .. "\n原图：" .. item.url
     log.info("url=%s, text=%s", url, text)
